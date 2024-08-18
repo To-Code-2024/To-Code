1ae4e3af4d6caac34fd834e81c231661b761a9a9
To-Code-2024 <toquode@gmail.com>
2024-08-18 19:17:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GeneratorTool.Examples;
+using Newtonsoft.Json;
+
+
+// use blog or survey model as examples
+// put the generated json in try it out page in the portal editor under this link https://victorious-glacier-0ea93880f.5.azurestaticapps.net/ide
+// then press sumbit to genrate and download the solution as zip file
+
+var json = JsonConvert.SerializeObject(Blog.SolutionModel);
+
+Console.WriteLine(json);
+Console.Read();
